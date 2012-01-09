a20f53eb516d941c3063e5a4e02fc6d41ec2242a
David (@ LBL) <dgquintas@gmail.com>
2012-01-09 11:29:47 -0800

    added support for throttling

--- caa/__init__.py
+++ caa/__init__.py
@@ -13,6 +13,10 @@ class SubscriptionMode(object):
 
     @classmethod
     def parse(cls, d):
+        """ Creates an instance from the description in dictionary `d`.
+        
+            This operation is the complementary of :meth:`as_dict`
+        """
         mode_name = d['mode']
         try:
             mode_cls = getattr(cls, mode_name)
@@ -22,27 +26,44 @@ class SubscriptionMode(object):
         return instance
     @classmethod
     def register(cls, mode):
+        """ Register `mode`, a subclass of `SubscriptionMode`. 
+        
+            This makes the mode subclass accessible as 
+            `SubscriptionMode.<modename>`. For instance, `SubscriptionMode.Monitor` 
+            for a mode subclass named 'Monitor' 
+        """
         cls.available_modes.append(mode)
         setattr(cls, mode.name, mode)
 
     def as_dict(self):
+        """ Abstract method. Must return a serializable representation 
+            of the instance.
+
+            The keys must correspond to parameters of the subclass' constructor,
+            as seen in :meth:`parse .
+        """
         raise NotImplemented("Subclasses must implement 'as_dict' method")
  
     def __repr__(self):
         return self.__str__()
 
 class _Monitor(SubscriptionMode):
-    __slots__ = ('delta',)
+    __slots__ = ('delta','max_freq')
     name = 'Monitor'
 
-    def __init__(self, delta=0.0, **dummy):
+    def __init__(self, delta=0.0, max_freq=None, **dummy):
+        """ 
+            :param delta: by how much incoming values must differ from the last archived one.
+            :param max_freq: maximum archiving frequency. Data arriving faster than this is discarded.
+        """
         self.delta = delta
+        self.max_freq = max_freq
 
     def as_dict(self):
-        return {'mode': self.name, 'delta': self.delta}
+        return {'mode': self.name, 'delta': self.delta, 'max_freq': self.max_freq}
 
     def __str__(self):
-        return 'Monitor(delta=%s)' % self.delta
+        return 'Monitor(delta=%s, max_freq=%s)' % (self.delta, self.max_freq)
 
 
 class _Scan(SubscriptionMode):
@@ -50,6 +71,9 @@ class _Scan(SubscriptionMode):
     name = 'Scan'
 
     def __init__(self, period=0.0, **dummy):
+        """
+            :param period: archiving period 
+        """
         self.period = period
 
     def as_dict(self):
