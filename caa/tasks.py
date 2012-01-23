c749d114f0955ac1b6daed56e08a52e23654dfee
David Garcia Quintas <dgquintas@gmail.com>
2012-01-23 17:07:22 +0100

    refactored workers and timers to their own module (tasks.py)

---
+++ caa/tasks.py
@@ -0,0 +1,266 @@
+import multiprocessing
+from multiprocessing import cpu_count, Process, current_process
+import threading
+import uuid
+from zlib import adler32
+try: 
+    import queue
+except:
+    import Queue as queue
+
+import time
+import collections
+import logging
+
+import caa.config as config
+
+logger = logging.getLogger('tasks')
+
+class Task(object):
+    """ Encapsulates a function alongside its arguments.
+    
+        In order to run it, simply call it without any arguments::
+
+            t = Task('the name', f, (arg1, arg2...))
+            t() # run it
+        
+    """
+    def __init__(self, name, f, *args):
+        """ :param name: A textual id for the task
+            :param f: The function to invoke when calling the task
+            :param args: tuple of arguments to be passed to `f`
+        """
+
+        self.name = name
+        self.f = f
+        self.args = args
+        self.done = False
+
+    def __call__(self, state):
+        return self.f(state, *self.args)
+
+    def __repr__(self):
+        return 'Task<%s(%r)>. Done: %s' % (self.name, self.args, self.done)
+
+
+#######################################################
+
+class TimerThread(threading.Thread):
+    """ A thread that takes care of invoking periodic events """
+
+    STOP_SENTINEL = '__STOP'
+
+    def __init__(self, inq, name):
+        """ :param inq: synchronized LIFO queue where :class:`Task` instances are
+                        submitted in order to add them to the timer.
+            :param name: a textual name for the timer thread.
+        """
+        threading.Thread.__init__(self, name=name)
+        self._inq = inq
+        self.tasks = []
+
+    def run(self):
+        while True:
+            t = time.time()
+            try:
+                item = self._inq.get(block=True, timeout=0.1)
+                logger.debug("Gotten item %s", item)
+                if item == self.STOP_SENTINEL:
+                    break
+                else:
+                    reqid, task, period = item
+                    logger.debug("Adding task '%s' with id '%s' and period '%f'", 
+                            task, reqid, period)
+                    self.tasks.append([t,task, period])
+
+            except queue.Empty: 
+                pass
+
+            # traverse task list checking for ripe ones
+            for i, (enq_t, task, period) in enumerate(self.tasks):
+                if t - enq_t >= period:
+                    # submit it to the wokers
+                    workers.request(task)
+                    # update enqueue time 
+                    self.tasks[i][0] = t
+
+        logger.info("Timer %s exiting: ", threading.current_thread().name)
+
+##############################################
+
+
+class TimersPool(object):
+    """ Pool of :class:`TimerThread`s """
+
+    def __init__(self, num_timers=2):
+        names = ['Timer-%d' % i for i in range(num_timers)]
+        self._inq = dict( (name, queue.Queue()) for name in names )
+
+        self._timers = []
+        for name in names:
+            q = self._inq[name]
+            self._timers.append( TimerThread(q, name) )
+
+        self._running = False
+
+    @property
+    def running(self):
+        return self._running
+
+    @property
+    def num_timers(self):
+        return len(self._timers)
+
+    def _submit(self, task, period):
+        if not self._running:
+            self.start()
+            self._running = True
+
+        reqid = uuid.uuid4()
+        # calculate the hash for the task based on its name
+        h = adler32(task.name) % self.num_timers
+        chosen_timer = self._timers[h]
+        logger.info("Enqueuing task '%s' in '%s'\'s queue", task.name, chosen_timer.name)
+        self._inq[chosen_timer.name].put((reqid, task, period))
+
+        return reqid
+
+    def request(self, task, period):
+        """ Add `task` to one of the pool's timers. 
+        
+            The given `task` will be run every `period` seconds.
+
+            :param task: the task to add to one of the pool's timers.
+            :param period: a float representing the period for `task`, in seconds (or fractions thereof).
+        """
+        logger.debug("Submitting periodic request '%s' with period %f", task, period)
+        # submit subscription request to queue
+        return self._submit(task, period)
+
+    def start(self):
+        """ Start all the pool's :class:`TimerThread`s """
+        if self._running: 
+            logger.warn("Timers already running.")
+        else:
+            for t in self._timers:
+                t.daemon = True
+                logger.info("Starting timer %s", t.name)
+                t.start()
+
+    def stop(self):
+        """ Stop all the pool's :class:`TimerThread`s """
+        if not self._running:
+            logger.warn("Timers weren't running.")
+        else:
+            logger.info("Stopping timers...")
+            [ q.put(TimerThread.STOP_SENTINEL) for q in self._inq.itervalues() ]
+            self._running = False
+
+#################################
+
+class WorkersPool(object):
+    """ Pool of :class:`Process`es.
+    
+        They manage PV's callbacks as well as ``get`` requests from the 
+        scanned PVs.
+    """
+
+    STOP_SENTINEL = '__STOP'
+
+    def __init__(self, num_workers=cpu_count()):
+        self._done_queue = multiprocessing.Queue()
+        self._workers = [ Process(target=self._worker, name=("Worker-%d"%i)) for i in range(num_workers) ]
+        self._task_queues = dict( (w.name, multiprocessing.JoinableQueue()) for w in self._workers  )
+        self._running = False
+        
+    @property
+    def running(self):
+        return self._running
+
+    @property
+    def num_workers(self):
+        return len(self._workers)
+
+    def _submit(self, task):  
+        if not self._running:
+            self.start()
+            self._running = True
+
+        reqid = uuid.uuid4()
+        # calculate the hash for the task based on its name
+        h = adler32(task.name) % self.num_workers
+        chosen_worker = self._workers[h]
+        self._task_queues[chosen_worker.name].put((reqid, task))
+
+        return reqid
+
+    def request(self, task):
+        """ Submit a :class:`Task` instance to be run by one of the processes """
+        logger.debug("Submitting request '%s'", task)
+        # submit subscription request to queue
+        return self._submit(task)
+
+    def _worker(self):
+        wname = current_process().name
+        logger.debug("At worker function '%s'", wname)
+        state = collections.defaultdict(dict)
+        q = self._task_queues[wname]
+        for reqid, task in iter(q.get, self.STOP_SENTINEL):
+            logger.debug("Worker '%s' processing task '%s' with id '%s'", 
+                    wname, task, reqid)
+            
+            t_res = task(state[task.name])  
+
+            task.result = t_res
+            logger.debug("Task with id '%s' for PV '%s' completed. Result: '%r'", \
+                    reqid, task.name, task.result )
+            self._done_queue.put((reqid, task))
+            q.task_done()
+
+            if not state[task.name]:
+                del state[task.name]
+        logger.info("Worker %s exiting: ", current_process().name)
+
+
+    def get_result(self, block=True, timeout=None):
+        """ Returns the receipt and *a* :class:``Task`` instance with 
+            a populated ``result`` attribute. 
+
+            :param boolean block: If ``True``, will block until a result in available. Otherwise,
+            return an item if one is immediately available, else raise :exc:`Queue.Empty`.
+            :param float timeout: If blocking, how many seconds to wait. If no result 
+            is available after that many seconds, :exc:`Queue.Empty` exception is raised.
+            :rtype: a list of the form ``(receipt, :class:`Task`-instance)``
+        """
+        return self._done_queue.get(block, timeout)
+ 
+    def join(self):
+        """ Blocks until all pending requests have finished """
+        logger.debug("Blocking until all pending requests finish.")
+        [ q.join for q in self._task_queues.itervalues() ]
+
+
+    def start(self):
+        if self._running: 
+            logger.warn("Workers already running.")
+        else:
+            for w in self._workers:
+                w.daemon = True
+                logger.info("Starting worker %s", w.name)
+                w.start()
+
+    def stop(self):
+        if not self._running:
+            logger.warn("Workers weren't running.")
+        else:
+            logger.info("Stopping workers...")
+            [ q.put(self.STOP_SENTINEL) for q in self._task_queues.itervalues() ]
+            self._running = False
+
+
+##############################################
+
+workers = WorkersPool(config.CONTROLLER['num_workers'])
+timers = TimersPool(config.CONTROLLER['num_timers'])
+
+
