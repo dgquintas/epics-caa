139ad3d82c2b2a9e3f02b6cacb5e24901d494db3
David (@ LBL) <dgquintas@gmail.com>
2012-03-05 17:53:47 -0800

    added "SUDO_ASKPASS". needed by iptables for tests

---
+++ envForTests.sh
@@ -0,0 +1,8 @@
+export PATH=$PATH:/home/dqgarcia/LBL/epics/base-3.14.12.1/bin/linux-x86_64/
+export LD_LIBRARY_PATH=/home/dqgarcia/LBL/epics/base-3.14.12.1/lib/linux-x86_64/
+export PYTHONPATH=$PWD
+export SUDO_ASKPASS=/usr/lib/ssh/x11-ssh-askpass
+
+#export EPICS_CA_ADDR_LIST=192.168.1.1
+#export EPICS_CA_AUTO_ADDR_LIST=no
+
