704111e100da04e64b8f07ced7e2065ce1e1a44a
David (@ LBL) <dgquintas@gmail.com>
2012-05-18 12:51:26 -0700

    updated tests

--- tests/test_controller.py
+++ tests/test_controller.py
@@ -48,7 +48,7 @@ class TestController(unittest.TestCase):
 
     def test_get_statuses(self):
         # connect
-        futures = [controller.subscribe(pv, SubscriptionMode.Monitor()) for pv in self.pvs]
+        futures = [controller.subscribe(pv, SubscriptionMode.Monitor(delta=0.01)) for pv in self.pvs]
         [ fut.wait() for fut in futures ]
         
         conn_ts = datastore._get_timestamp_ms()
@@ -82,7 +82,9 @@ class TestController(unittest.TestCase):
             
         for fakepv in self.fake_pvs:
             statuses = controller.get_statuses(fakepv)
-            self.assertEqual(statuses, [])
+            # they must be there, with 'connected' set to false
+            for status in statuses:
+                self.assertFalse(status['connected'])
 
     def test_get_values(self):
         self.test_subscribe()
@@ -93,35 +95,43 @@ class TestController(unittest.TestCase):
         time.sleep(wait_for) # ioc generates values at 1 Hz
         expected_num_values = wait_for * ioc_freq
 
+        # doesn't exist AND never subscribed
         no_values = controller.get_values('test:doesntexist')
-        self.assertEqual(no_values, [])
+        self.assertEqual(no_values['rows'], [])
+        self.assertEqual(no_values['meta']['total'], 0)
 
         for longpv in self.long_pvs:
             values = controller.get_values(longpv, limit=expected_num_values)
-            logger.info("Received %d values after %d seconds at %d Hz", len(values), wait_for, ioc_freq)
-            self.assertEqual(len(values), expected_num_values)
+            rows = values['rows']
+            logger.info("Received %d values after %d seconds at %d Hz", len(rows), wait_for, ioc_freq)
+            self.assertEqual(len(rows), expected_num_values)
             # we don't really know how for from a change the PV is. We 
             # can only guarantee that we'll get expected_num_values 
             # updates during our sleep period
-            first_value = int(values[0]['value'])
-            for (i,value) in enumerate(values):
+            first_value = int(rows[0]['value'])
+            for (i,value) in enumerate(rows):
                 self.assertEqual(longpv, value['pvname'])
                 self.assertEqual('long', value['type'])
                 self.assertEqual(first_value-i, int(value['value']))
 
         for doublepv in self.double_pvs:
             values = controller.get_values(doublepv, limit=expected_num_values)
-            logger.info("Received %d values after %d seconds at %d Hz", len(values), wait_for, ioc_freq)
-            self.assertEqual(len(values), expected_num_values)
-            first_value = float(values[0]['value'])
-            for (i,value) in enumerate(values):
+            rows = values['rows']
+            logger.info("Received %d values after %d seconds at %d Hz", len(rows), wait_for, ioc_freq)
+            self.assertEqual(len(rows), expected_num_values)
+            first_value = float(rows[0]['value'])
+            print(first_value)
+            for (i,value) in enumerate(rows):
                 self.assertEqual(doublepv, value['pvname'])
                 self.assertEqual('double', value['type'])
                 self.assertEqual(first_value-i, float(value['value']))
 
         for fakepv in self.fake_pvs:
             values = controller.get_values(fakepv)
-            self.assertEqual([], values)
+            # they don't exist but we subscribed to them. Expect entries with null values
+            rows = values['rows']
+            for row in rows:
+                self.assertIsNone(row['value'])
 
 
     def test_get_values_by_date(self):
@@ -130,24 +140,34 @@ class TestController(unittest.TestCase):
         wait_for = 5
         start = datetime.datetime.now()
         halftime = start + datetime.timedelta(seconds=wait_for/2.0)
+        halftime_ts = time.mktime( halftime.timetuple())
  
         time.sleep(wait_for) 
 
         # test "up to" semantics, open interval on the left
         for pvset in (self.long_pvs, self.double_pvs):
             for pv in pvset:
-                all_values = controller.get_values(pv)
-                upto_values = controller.get_values(pv, to_date=halftime) # first half
-                from_values = controller.get_values(pv, from_date=halftime) # second half
+                all_values = controller.get_values(pv, limit=2**30)
+                upto_values = controller.get_values(pv, to_date=halftime, limit=2**30) # first half
+                from_values = controller.get_values(pv, from_date=halftime, limit=2**30) # second half
                 ts = time.mktime( halftime.timetuple())
-                for v in upto_values:
+                for v in upto_values['rows']:
                     self.assertLessEqual(v['timestamp'], ts)
-                for v in from_values:
+                for v in from_values['rows']:
                     self.assertGreaterEqual(v['timestamp'], ts)
+
+                # it may happen that new values arrive between the gathering of values and the
+                # counting colums operation
+                self.assertLessEqual(len(all_values['rows']), all_values['meta']['total'])
+                self.assertLessEqual(len(upto_values['rows']), upto_values['meta']['total'])
+                self.assertLessEqual(upto_values['meta']['last_ts'], halftime_ts)
+                self.assertGreaterEqual(from_values['meta']['first_ts'], halftime_ts)
+                self.assertLessEqual(len(from_values['rows']), from_values['meta']['total'])
+
 
                 # it's >= because there may be some overlap, due to the loss of precision in the
                 # timestamp we use in the get_values call
-                self.assertGreaterEqual( len(upto_values)+len(from_values), len(all_values) )
+                self.assertGreaterEqual( len(upto_values['rows'])+len(from_values['rows']), len(all_values['rows']) )
 
     def test_get_values_throttled(self):
         # give it time to gather some data
@@ -155,30 +175,26 @@ class TestController(unittest.TestCase):
         ioc_freq = 10 # Hz
         max_archiving_freq = 5 # Hz
 
-        mode = SubscriptionMode.Monitor(max_freq=max_archiving_freq)
+        mode = SubscriptionMode.Monitor(delta=0.01,max_freq=max_archiving_freq)
         self.test_subscribe(mode)
 
         time.sleep(wait_for) 
 
         no_values = controller.get_values('test:doesntexist')
-        self.assertEqual(no_values, [])
+        self.assertEqual(no_values['rows'], [])
 
         for pvset in (self.long_pvs, self.double_pvs):
             for pv in pvset:
                 values = controller.get_values(pv)
                 logger.info("Received %d values for '%s' after %d seconds at %d Hz", \
-                        len(values), pv, wait_for, max_archiving_freq)
+                        len(values['rows']), pv, wait_for, max_archiving_freq)
 
                 # diferences in timestamp shouldn't exceed 1/max_archiving_freq (period) 
                 max_period = 1/max_archiving_freq
-                for i in range(len(values)-1):
-                    ith = values[i]['timestamp']
-                    ip1th = values[i+1]['timestamp'] 
+                for i in range(len(values['rows'])-1):
+                    ith = values['rows'][i]['timestamp']
+                    ip1th = values['rows'][i+1]['timestamp'] 
                     self.assertGreaterEqual( ith-ip1th, max_period) 
-
-        for fakepv in self.fake_pvs:
-            values = controller.get_values(fakepv)
-            self.assertEqual([], values)
 
     def test_get_values_delta(self):
         # give it time to gather some data
@@ -192,25 +208,22 @@ class TestController(unittest.TestCase):
         time.sleep(wait_for) 
 
         no_values = controller.get_values('test:doesntexist')
-        self.assertEqual(no_values, [])
+        self.assertEqual(no_values['rows'], [])
 
         for pvset in (self.long_pvs, self.double_pvs):
             for pv in pvset:
                 values = controller.get_values(pv)
+                rows = values['rows']
                 logger.info("Received %d values for '%s' after %d seconds", \
-                        len(values), pv, wait_for)
-                for i in range(len(values)-1):
-                    ith = values[i]['value']
-                    ip1th = values[i+1]['value'] 
+                        len(rows), pv, wait_for)
+                for i in range(len(rows)-1):
+                    ith = rows[i]['value']
+                    ip1th = rows[i+1]['value'] 
                     self.assertGreaterEqual( ith-ip1th, delta) 
-
-        for fakepv in self.fake_pvs:
-            values = controller.get_values(fakepv)
-            self.assertEqual([], values)
 
     def test_get_values_disconnected(self):
         conn_ts = datastore._get_timestamp_ms()
-        futures = [ controller.subscribe(pv, SubscriptionMode.Monitor()) \
+        futures = [ controller.subscribe(pv, SubscriptionMode.Monitor(delta=0.01)) \
                      for pv in self.existing_pvs ]
         results = [ fut.get() for fut in futures ]
         time.sleep(2)
@@ -224,8 +237,8 @@ class TestController(unittest.TestCase):
             self.assertTrue(status['connected'])
 
             last_values = controller.get_values(pv)
-            self.assertTrue(last_values)
-            last_value = last_values[0]
+            self.assertTrue(last_values['rows'])
+            last_value = last_values['rows'][0]
             self.assertGreater(last_value['archived_at_ts'], conn_ts)
             self.assertGreater(last_value['value'], 1)
 
@@ -242,8 +255,8 @@ class TestController(unittest.TestCase):
             self.assertFalse(status['connected'])
 
             last_values = controller.get_values(pv)
-            self.assertTrue(last_values)
-            last_value = last_values[0]
+            self.assertTrue(last_values['rows'])
+            last_value = last_values['rows'][0]
             self.assertGreater(last_value['archived_at_ts'], predisconn_ts)
             self.assertGreater(postdisconn_ts, last_value['archived_at_ts'])
             self.assertIsNone(last_value['value'])
@@ -252,7 +265,7 @@ class TestController(unittest.TestCase):
 
 
     def test_subscribe(self, mode = None):
-        mode = mode or SubscriptionMode.Monitor()
+        mode = mode or SubscriptionMode.Monitor(delta=0.01)
         futures = [ controller.subscribe(pv, mode) for pv in self.pvs ]
         results = [fut.get() for fut in futures]
         
@@ -263,7 +276,7 @@ class TestController(unittest.TestCase):
             self.assertIn(pv, pvs)
 
     def test_msubscribe(self ):
-        mode_choices = [SubscriptionMode.Monitor(), SubscriptionMode.Scan(period=1)]
+        mode_choices = [SubscriptionMode.Monitor(delta=0.01), SubscriptionMode.Scan(period=1)]
         modes = [random.choice(mode_choices) for _ in self.pvs]
         futures = controller.msubscribe( self.pvs, modes )
 
@@ -287,7 +300,7 @@ class TestController(unittest.TestCase):
 
     def test_subscribe_scan(self):
         for pv in self.pvs[:4]:
-            self.assertEqual(len(controller.get_values(pv)), 0)
+            self.assertEqual(len(controller.get_values(pv)['rows']), 0)
 
         periods = (1,1.5, 2, 2.1)
         for pv,p in zip(self.pvs[:4], periods):
@@ -297,7 +310,7 @@ class TestController(unittest.TestCase):
         time.sleep(sleep_for)
         
         for pv,p in zip(self.pvs[:4], periods):
-            res = controller.get_values(pv)
+            res = controller.get_values(pv)['rows']
             self.assertAlmostEqual(sleep_for/p, len(res), delta=1)
 
     def test_constant_value_scan(self):
@@ -307,17 +320,18 @@ class TestController(unittest.TestCase):
         time.sleep(4)
 
         values = controller.get_values(self.nonchanging_pvs[0])
-        self.assertGreaterEqual(len(values), 3)
-        self.assertLessEqual(len(values), 4)
-        last_value = values[-1] # ie, oldest
-        for i,value in enumerate(reversed(values[:-1])):
+        rows = values['rows']
+        self.assertGreaterEqual(len(rows), 3)
+        self.assertLessEqual(len(rows), 4)
+        last_value = rows[-1] # ie, oldest
+        for i,value in enumerate(reversed(rows[:-1])):
             self.assertAlmostEqual(value['archived_at_ts']-(i+1)*1e6, last_value['archived_at_ts'], delta=0.1*1e6)
             self.assertEqual(value['timestamp'], last_value['timestamp'])
         
 
     def test_unsubscribe_monitor(self):
         pvname = self.long_pvs[0]
-        controller.subscribe(pvname, SubscriptionMode.Monitor())
+        controller.subscribe(pvname, SubscriptionMode.Monitor(delta=0.01))
 
         #check that the system has registered the subscription
         apv = controller.get_apv(pvname)
@@ -331,7 +345,7 @@ class TestController(unittest.TestCase):
         self.assertTrue(unsub_req)
         unsub_req.wait()
         # take note of the data present
-        before = controller.get_values(self.long_pvs[0])
+        before = controller.get_values(self.long_pvs[0])['rows']
 
         # verify that the system has removed the pv
         apv = controller.get_apv(self.long_pvs[0])
@@ -339,7 +353,7 @@ class TestController(unittest.TestCase):
 
         # wait to see if we are still receiving data 
         time.sleep(2)
-        after = controller.get_values(self.long_pvs[0])
+        after = controller.get_values(self.long_pvs[0])['rows']
 
         # we shouldn't have received any data during the last sleep period
         self.assertEqual(len(before), len(after))
@@ -349,7 +363,7 @@ class TestController(unittest.TestCase):
         self.assertIsNone(controller.unsubscribe('foobar')) 
 
     def test_munsubscribe(self):
-        controller.subscribe('DEFAULT.PV', SubscriptionMode.Monitor())
+        controller.subscribe('DEFAULT.PV', SubscriptionMode.Monitor(delta=0.01))
         controller.subscribe('FOO.PV', SubscriptionMode.Monitor(delta=0.123))
         controller.subscribe('BAR.PV', SubscriptionMode.Scan(period=123.1) )
         
@@ -399,7 +413,7 @@ class TestController(unittest.TestCase):
         def just_the_name(list_of_pairs):
             return map(operator.itemgetter(0), list_of_pairs)
 
-        controller.subscribe('DEFAULT.PV', SubscriptionMode.Monitor()).wait()
+        controller.subscribe('DEFAULT.PV', SubscriptionMode.Monitor(delta=0.01)).wait()
         controller.subscribe('FOO.PV', SubscriptionMode.Monitor(delta=0.123)).wait()
         controller.subscribe('BAR.PV', SubscriptionMode.Scan(period=123.1) ).wait()
         
@@ -413,7 +427,7 @@ class TestController(unittest.TestCase):
         self.assertEqual( monitor_pvs, ['DEFAULT.PV', 'FOO.PV'])
     
     def test_list_archived_only(self):
-        controller.subscribe('test:long1', SubscriptionMode.Monitor()).wait()
+        controller.subscribe('test:long1', SubscriptionMode.Monitor(delta=0.01)).wait()
         controller.subscribe('test:long2', SubscriptionMode.Monitor(delta=0.123)).wait()
         controller.subscribe('test:long3', SubscriptionMode.Scan(period=123.1) ).wait()
     
@@ -428,7 +442,7 @@ class TestController(unittest.TestCase):
 
 
     def test_list_subscribed_only(self):
-        controller.subscribe('DEFAULT.PV', SubscriptionMode.Monitor()).wait()
+        controller.subscribe('DEFAULT.PV', SubscriptionMode.Monitor(delta=0.01)).wait()
         controller.subscribe('FOO.PV', SubscriptionMode.Monitor(delta=0.123)).wait()
         controller.subscribe('BAR.PV', SubscriptionMode.Scan(period=123.1) ).wait()
 
@@ -445,7 +459,7 @@ class TestController(unittest.TestCase):
         self.assertItemsEqual(arched.keys(), ['FOO.PV'])
 
     def test_list_and_get_pvs(self):
-        monitor_mode = SubscriptionMode.Monitor()
+        monitor_mode = SubscriptionMode.Monitor(delta=0.01)
         scan_mode = SubscriptionMode.Scan(period=1)
         now_ts = time.time() * 1e6
         num_pvs = len(self.pvs)
@@ -483,7 +497,7 @@ class TestController(unittest.TestCase):
 
 
     def test_get_apvs(self):
-        controller.subscribe('DEFAULT.PV', SubscriptionMode.Monitor())
+        controller.subscribe('DEFAULT.PV', SubscriptionMode.Monitor(delta=0.01))
         controller.subscribe('FOO.PV', SubscriptionMode.Monitor(delta=0.123))
         controller.subscribe('BAR.PV', SubscriptionMode.Scan(period=123.1) )
 
@@ -499,7 +513,7 @@ class TestController(unittest.TestCase):
         self.assertEqual( singleapv, defaultapv)
         self.assertEqual( defaultapv.name, 'DEFAULT.PV' )
         self.assertEqual( defaultapv.mode.name, SubscriptionMode.Monitor.name )
-        self.assertEqual( defaultapv.mode.delta, 0.0 )
+        self.assertEqual( defaultapv.mode.delta, 0.01 )
 
         fooapv = all_apvs['FOO.PV']
         self.assertEqual( fooapv.name, 'FOO.PV' )
@@ -516,7 +530,7 @@ class TestController(unittest.TestCase):
         self.assertRaises(TypeError, controller.get_apvs, 'not a collection')
 
     def test_save_config(self):
-        controller.subscribe('DEFAULT.PV', SubscriptionMode.Monitor())
+        controller.subscribe('DEFAULT.PV', SubscriptionMode.Monitor(delta=0.01))
         controller.subscribe('FOO.PV',  SubscriptionMode.Monitor(0.123))
         controller.subscribe('BAR.PV', SubscriptionMode.Scan(123.1)) 
 
@@ -527,7 +541,7 @@ class TestController(unittest.TestCase):
         pvnames = ('DEFAULT.PV', 'FOO.PV', 'BAR.PV')
         
         futures = []
-        futures.append(controller.subscribe('DEFAULT.PV', SubscriptionMode.Monitor()))
+        futures.append(controller.subscribe('DEFAULT.PV', SubscriptionMode.Monitor(delta=0.01)))
         futures.append(controller.subscribe('FOO.PV',  SubscriptionMode.Monitor(0.123)))
         futures.append(controller.subscribe('BAR.PV', SubscriptionMode.Scan(123.1)))
 
@@ -552,7 +566,7 @@ class TestController(unittest.TestCase):
         av = controller.get_apv('DEFAULT.PV')
         self.assertEqual( av.name, 'DEFAULT.PV' )
         self.assertEqual( av.mode.name, SubscriptionMode.Monitor.name )
-        self.assertEqual( av.mode.delta, 0.0 )
+        self.assertEqual( av.mode.delta, 0.01 )
 
         av = controller.get_apv('FOO.PV')
         self.assertEqual( av.name, 'FOO.PV' )
